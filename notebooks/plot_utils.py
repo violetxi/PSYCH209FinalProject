13b2d82f73a547de5b039f78d80f88f03d79734e
Violet (Ziyu) Xiang <ziyxiang@node12-ccncluster.stanford.edu>
2021-03-04 20:37:33 -0800

    loss visualization

---
+++ notebooks/plot_utils.py
@@ -0,0 +1,14 @@
+import torch
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+
+def plot_loss(ckpt_path):
+    which_model = ckpt_path.split('_')[0]
+    checkpoint = torch.load(ckpt_path)
+    losses = checkpoint['losses']    
+    plt.plot(losses)
+    plt.xlabel('Number of steps')
+    plt.ylabel('Loss (cosine dist)')
+    plt.title(f'{which_model} loss curve')
